3170f9c5a95a71b0a8fafde3973519241c3da6d3
GravleyCM <GravleyCM@gmail.com>
2022-03-28 14:51:13 -0400

    add Saturday to Schema

--- models/timeTable.js
+++ models/timeTable.js
@@ -89,6 +89,23 @@ const timeTableSchema = Schema({
       DestinationStation: String,
     }]
     }],
+    Saturday: [{
+      OpeningTime: String,
+      FirstTrain: [{
+        Time: String,
+        DestinationStation: String
+      }, {
+      Time: String,
+      DestinationStation: String
+    }],
+    LastTrain: [{
+      Time: String,
+      DestinationStation: String,
+    }, {
+      Time: String,
+      DestinationStation: String,
+    }]
+    }],
 },{
   timestamp: true
 })

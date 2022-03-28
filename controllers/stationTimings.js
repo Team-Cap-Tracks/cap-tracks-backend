fb528db958b60b5321cf3977444d90b8416187eb
GravleyCM <GravleyCM@gmail.com>
2022-03-28 15:28:19 -0400

    fix capitalization

--- controllers/stationTimings.js
+++ controllers/stationTimings.js
@@ -1,7 +1,13 @@
-import { timeTable } from '../models/timeTable.js'
+import { TimeTable } from '../models/timeTable.js'
 
 function index() {
-
+  TimeTable.find({})
+    .then(line => {
+      res.json(line)
+    })
+    .catch(err => {
+      res.json(err)
+    })
 }
 
 export {

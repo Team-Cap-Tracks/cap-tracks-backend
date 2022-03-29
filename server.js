1cce6ecc7cdebbc1fb2429703fb43dd0897efdbd
Caleb Vietinghoff <vietinghoffcaleb@gmail.com>
2022-03-29 09:39:35 -0400

    update router to correct path

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.json())
 app.use('/api/profiles', profilesRouter)
 app.use('/api/auth', authRouter)
 app.use('/api/lines', linesRouter)
-app.use('/api/stationtimings', stationTimingsRouter)
+app.use('/api/timetables', stationTimingsRouter)
 
 app.use(function (req, res, next) {
   res.status(404).json({ err: "Not found" })

a1d61b8dfa366aea0546e58b10af086cedc955ad
Caleb Vietinghoff <vietinghoffcaleb@gmail.com>
2022-03-31 08:25:04 -0400

    finish index function

--- controllers/tickets.js
+++ controllers/tickets.js
@@ -1,7 +1,14 @@
 import { Ticket } from '../models/user.js'
 
 function index(req, res) {
-  
+  Ticket.find({})
+  .populate('owner')
+  .then(tickets => {
+    res.json(tickets)
+  })
+  .catch(err => {
+    res.json(err)
+  })
 }
 
 export {
